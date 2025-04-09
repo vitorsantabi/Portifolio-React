642e887a3b5fae0f03045e2343b4c9b054faf7c9
Jose Vitor <98997905+vitorsantabi@users.noreply.github.com>
2025-04-09 19:49:08 -0300

    att

--- components/pages/projetos/Projetos.jsx
+++ components/pages/projetos/Projetos.jsx
@@ -2,7 +2,7 @@ export default function Projetos() {
     return (
       <div className="p-6">
         <h1 className="text-3xl font-bold mb-4">Projetos</h1>
-        <p>Aqui estão alguns dos projetos que desenvolvi recentemente.</p>
+        <p>Pagina em construção....</p>
       </div>
     );
   }
--- components/pages/redes/Redes.jsx
+++ components/pages/redes/Redes.jsx
@@ -1,21 +1,42 @@
 import React from 'react';
-import X from "../../../public/twitter.png"
-import Y from "../../../public/github-brands-solid.svg"
-import Z from "../../../public/linkedin-logo.png"
-import W from "../../../public/instagram.png"
-import "./style.css"; 
+import X from "../../../public/twitter.png";
+import Y from "../../../public/github-brands-solid.svg";
+import Z from "../../../public/linkedin-logo.png";
+import W from "../../../public/instagram.png";
+import "./styleRedes.css";
+
 export default function Redes() {
-    return (
-      <div className="p-6">
-        <h1 className="text-3xl font-bold mb-4">Redes</h1>
-        <p>Me siga nas redes sociais:</p>
-        <ul className="icons-redes">
-          <li><img src={Y} alt="" /><a href="https://github.com/vitorsantabi" target="_blank" className="text-blue-500 hover:underline">GitHub</a></li>
-          <li><img src={Z} alt="" /><a href="https://www.linkedin.com/in/vitorsantab/" target="_blank" className="text-blue-500 hover:underline">LinkedIn</a></li>
-          <li><img src={W} alt="" /><a href="https://www.instagram.com/vitorsantab/" target="_blank" className="text-blue-500 hover:underline">Instagram</a></li>
-          <li><img src={X} alt="" /><a href="https://x.com/vitorsantab" target="_blank" className="text-blue-500 hover:underline">X</a></li>
-        </ul>
-      </div>
-    );
-  }
+  return (
+    <div className="content-redes">
+      <h1 className="title">Redes Sociais</h1>
+      <p>Me siga nas redes sociais:</p>
+      <ul className="icons-redes">
+        <li className='iconRedes'>
+        
+          <a href="https://github.com/vitorsantabi" target="_blank" rel="noopener noreferrer" className="redes">
+            <img src={Y} alt="GitHub logo" />
+            GitHub</a>
+        </li>
+        <li className='iconRedes'>
+          
+          <a href="https://www.linkedin.com/in/vitorsantab/" target="_blank" rel="noopener noreferrer" className="redes">
+          <img src={Z} alt="LinkedIn logo" />
+          LinkedIn</a>
+        </li>
+        <li className='iconRedes'>
+          
+          <a href="https://www.instagram.com/vitorsantab/" target="_blank" rel="noopener noreferrer" className="redes">
+          <img src={W} alt="Instagram logo" />
+          Instagram</a>
+        </li>
+        <li className='iconRedes'>
+          <a href="https://x.com/vitorsantab" target="_blank" rel="noopener noreferrer" className="redes">
+          <img src={X} alt="X logo" />
+          X</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
   
